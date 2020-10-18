264d93d841eb96bcbf75a1213f9e27d4594369df
roni07 <mdmehedihasanroni28@gmail.com>
2020-10-18 19:26:54 +0600

    project title updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,22 +13,22 @@ const Header = () => {
                     <span className="my-navbar-brand">M</span>ehedi
                 </Link>
                 <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="ml-auto my-navbar-nav">
-                        <Link activeClass="active" spy={true} smooth={true} offset={-80} duration={500}
-                              to="home">Home</Link>
-                        <Link activeClass="active" spy={true} smooth={true} offset={-80} duration={500}
-                              to="about">About</Link>
-                        <Link activeClass="active" spy={true} smooth={true} offset={-80} duration={500}
-                              to="resume">Resume</Link>
-                        <Link activeClass="active" spy={true} smooth={true} offset={-80} duration={500}
-                              to="services">Services</Link>
-                        <Link activeClass="active" spy={true} smooth={true} offset={-80} duration={500}
-                              to="project">Project</Link>
-                        <Link activeClass="active" spy={true} smooth={true} offset={-80} duration={500}
-                              to="contact">Contact</Link>
-                    </Nav>
-                </Navbar.Collapse>
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ml-auto my-navbar-nav">
+                            <Link activeClass="active" spy={true} smooth={true} offset={-80} duration={500}
+                                  to="home">Home</Link>
+                            <Link activeClass="active" spy={true} smooth={true} offset={-80} duration={500}
+                                  to="about">About</Link>
+                            <Link activeClass="active" spy={true} smooth={true} offset={-80} duration={500}
+                                  to="resume">Resume</Link>
+                            <Link activeClass="active" spy={true} smooth={true} offset={-80} duration={500}
+                                  to="services">Services</Link>
+                            <Link activeClass="active" spy={true} smooth={true} offset={-80} duration={500}
+                                  to="project">Project</Link>
+                            <Link activeClass="active" spy={true} smooth={true} offset={-80} duration={500}
+                                  to="contact">Contact</Link>
+                        </Nav>
+                    </Navbar.Collapse>
             </Navbar>
 
         </header>
