3f1fd1d3df12e20d1c361134bc557dfe8b2c53f4
roni07 <mdmehedihasanroni28@gmail.com>
2020-10-22 00:01:46 +0600

    email service integrated

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,6 +2,9 @@ import * as React from 'react';
 import {Component} from 'react';
 import SectionTitle from "./common/SectionTitle";
 import {RiContactsLine, RiMailSendLine, RiMapPinLine, RiPhoneLine, RiRadioButtonLine} from "react-icons/ri";
+import emailjs from 'emailjs-com';
+import Swal from 'sweetalert2'
+
 
 import '../static/css/contact.css';
 import {getUser} from "../services/AboutService";
@@ -11,13 +14,56 @@ class Contact extends Component {
 
     state = {
         loading: true,
-        user: null
+        user: null,
+        mailData: {
+            fullName: "",
+            email: "",
+            subject: "",
+            message: ""
+        }
     }
 
     async componentDidMount() {
         getUser()
             .then(response => this.setState({user: response.data, loading: false}));
     }
+
+    onHandleChange = e => {
+        const {name, value} = e.target;
+
+        this.setState({...this.state, mailData: {...this.state.mailData, [name]: value}});
+    }
+
+
+    sendEmail = e => {
+        e.preventDefault();
+
+        emailjs.sendForm('gmail', 'template_i20p30b', e.target, 'user_QsaGrTVWRr0fFn02HpZ7R')
+            .then((result) => {
+                    console.log(result.text);
+                    Swal.fire({
+                        title: 'Your Message Has been Sent',
+                        icon: 'success',
+                        showConfirmButton: false,
+                        timer: 2000,
+                        position: "center"
+                    })
+                },
+                (error) => {
+                    console.log(error.text);
+                });
+
+        this.setState({
+            ...this.state, mailData: {
+                fullName: "",
+                email: "",
+                subject: "",
+                message: ""
+            }
+        });
+
+    }
+
 
     render() {
 
@@ -41,21 +87,29 @@ class Contact extends Component {
                         <div className="row">
                             <div className="col-lg-6 col-sm-12">
                                 <h5 className="contact-form-message">Message me</h5>
-                                <form className="contact-form">
+                                <form className="contact-form" onSubmit={this.sendEmail}>
                                     <div className="form-row">
                                         <div className="col-lg-6 col-sm-12 mb-3">
-                                            <input type="text" className="form-control" placeholder="Name"/>
+                                            <input type="text" className="form-control" placeholder="Name"
+                                                   name="fullName" value={this.state.mailData.fullName}
+                                                   onChange={this.onHandleChange}/>
                                         </div>
                                         <div className="col-lg-6 col-sm-12 mb-3">
-                                            <input type="text" className="form-control" placeholder="Email"/>
+                                            <input type="email" className="form-control" placeholder="Email"
+                                                   name="email" value={this.state.mailData.email}
+                                                   onChange={this.onHandleChange}/>
                                         </div>
 
                                         <div className="col-12 mb-3">
-                                            <input type="text" className="form-control" placeholder="Subject"/>
+                                            <input type="text" className="form-control" placeholder="Subject"
+                                                   name="subject" value={this.state.mailData.subject}
+                                                   onChange={this.onHandleChange}/>
                                         </div>
 
                                         <div className="col-12 mb-3">
-                                            <textarea className="form-control" placeholder="Message"/>
+                                            <textarea className="form-control" placeholder="Message" name="message"
+                                                      value={this.state.mailData.message}
+                                                      onChange={this.onHandleChange}/>
                                         </div>
                                     </div>
                                     <button type="submit" className="btn mb-2 form-btn">Send Message</button>
